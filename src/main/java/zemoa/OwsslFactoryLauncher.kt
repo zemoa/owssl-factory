0e97ed135d1d3ecf7572c76cd7f20025538abd3c
zemoa <zemoa@netc.fr>
2021-07-06 23:09:50 +0200

    speedup boot time

--- src/main/java/zemoa/OwsslFactoryLauncher.kt
+++ src/main/java/zemoa/OwsslFactoryLauncher.kt
@@ -1,11 +1,22 @@
 package zemoa
 
 import javafx.application.Application
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration
+import org.springframework.boot.autoconfigure.sql.init.SqlInitializationAutoConfiguration
+import org.springframework.boot.autoconfigure.task.TaskExecutionAutoConfiguration
+import org.springframework.boot.autoconfigure.task.TaskSchedulingAutoConfiguration
 import zemoa.OwsslFactoryApp
 
 @SpringBootApplication
+@EnableAutoConfiguration(
+    exclude=[
+        SqlInitializationAutoConfiguration::class,
+        TaskSchedulingAutoConfiguration::class,
+        TaskExecutionAutoConfiguration::class,
+        DataSourceTransactionManagerAutoConfiguration::class,
+    ])
 class OwsslFactoryLauncher
 
 fun main(args: Array<String>) {
